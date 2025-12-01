92fb6a66ff7ddf3d2d8b66a092fdba9e593ff0f0
Dmitriy Vassilyev <lotus33497@yandex.ru>
2025-12-01 14:38:16 +0700

    showed all beans

--- src/main/java/com/example/demo/DemoProjectApplication.java
+++ src/main/java/com/example/demo/DemoProjectApplication.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class DemoProjectApplication {
+public class DemoProjectApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
 
+	@Autowired
+	private ApplicationContext appContext;
+
+	@Override
+	public void run(String... args) {
+		String[] beans = appContext.getBeanDefinitionNames();
+		Arrays.sort(beans);
+		for (String bean : beans) {
+			System.out.println(bean + " of Type :: " + appContext.getBean(bean).getClass());
+		}
+	}
 }
