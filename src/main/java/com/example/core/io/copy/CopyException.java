6cc677b665d49913802b46f22c63df27885a6f20
Dmitriy Vassilyev <lotus33497@yandex.ru>
2025-12-04 23:55:50 +0700

    Copy added

---
+++ src/main/java/com/example/core/io/copy/CopyException.java
@@ -0,0 +1,7 @@
+package com.example.core.io.copy;
+
+public class CopyException extends RuntimeException {
+    public CopyException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/com/example/core/io/copy/CopyTest.java
@@ -0,0 +1,30 @@
+package com.example.core.io.copy;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CopyTest {
+
+    @Test
+    void copyNotExistingFile() {
+        assertThrows(CopyException.class, () -> Copy.copy("not-exist", "not-exist2"));
+    }
+
+    @Test
+    void copyTheSameFile() {
+        String from = "pom.xml";
+        String to = "pom2.xml.iml";
+        Copy.copy(from, to);
+        try {
+            assertArrayEquals(Files.readAllBytes(Path.of(to)), Files.readAllBytes(Path.of(from)));
+        } catch (IOException e) {
+            assert false;
+        }
+    }
+}
