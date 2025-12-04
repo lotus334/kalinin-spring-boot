d344c184cbdc93b0568d0abc0ae6043e41b5704d
Dmitriy Vassilyev <lotus33497@yandex.ru>
2025-12-04 22:21:57 +0700

    CustomSingleLinkedList added

---
+++ src/main/java/com/example/core/singleLinkedList/MyList.java
@@ -0,0 +1,14 @@
+package com.example.core.singleLinkedList;
+
+public interface MyList {
+
+    void add(int value);
+
+    boolean remove(int value);
+
+    boolean contains(int value);
+
+    void printList();
+
+    int getSize();
+}
