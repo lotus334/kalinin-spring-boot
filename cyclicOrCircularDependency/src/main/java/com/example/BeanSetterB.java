ddef32f2d51e9854607b3a0a0475ccd5b5732129
Dmitriy Vassilyev <lotus33497@yandex.ru>
2025-12-05 16:00:30 +0700

    cyclic or circular dependency correction

--- cyclicOrCircularDependency/src/main/java/com/example/BeanSetterB.java
+++ cyclicOrCircularDependency/src/main/java/com/example/BeanSetterB.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 
 /**
  * НЕ РАБОТАЕТ
+ * после spring framework 5 и boot 2.6 все эти хитрости с сеттером уже не работают. Spring заранее сканирует и запрещает это
+ * если хочешь проверить именно пример с сеттерами, то добавь в properties: spring.main.allow-circular-references=true
  */
 @Component
 public class BeanSetterB {
