628b8afa8069aea73e9845ba26f8498653732b35
Dmitriy Vassilyev <lotus33497@yandex.ru>
2025-12-16 18:18:31 +0700

    spring dev tools added

---
+++ springDevTools/src/main/java/com/springDevTools/DevToolsApp.java
@@ -0,0 +1,23 @@
+package com.springDevTools;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevToolsApp implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevToolsApp.class, args);
+    }
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        itemRepository.save(new Item(null, "Item 1"));
+        itemRepository.save(new Item(null, "Item 2"));
+    }
+}
